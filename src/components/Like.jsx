32a88f91045c7b11e74339946041b903c7b1e6f3
HasanAhmed987 <92812904+HasanAhmed987@users.noreply.github.com>
2022-01-23 17:26:40 +0500

    Update Like.jsx

--- src/components/Like.jsx
+++ src/components/Like.jsx
@@ -18,19 +18,3 @@ class Like extends Component {
 }
 
 export default Like;
-
-// function Like() {
-//   let classes = "fa fa-heart";
-//   if (!this.props.liked) classes += "-o";
-
-//   return (
-//     <i
-//       onClick={this.props.onClick}
-//       className={classes}
-//       style={{ cursor: "pointer" }}
-//       aria-hidden="true"
-//     />
-//   );
-// }
-
-// export default Like;
