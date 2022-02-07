2b5d5c337fe81fa86297c894ee3657c643e0d38b
HasanAhmed987 <92812904+HasanAhmed987@users.noreply.github.com>
2022-02-08 02:01:08 +0500

    Update Like.jsx

--- src/components/common/Like.jsx
+++ src/components/common/Like.jsx
@@ -1,20 +1,18 @@
 import React, { Component, useEffect, useState } from "react";
 
-function Like(props) {
-  let [classes, setClasses] = useState("fa fa-heart");
-
-  useEffect(() => {
-    if (!props.liked) setClasses((classes += "-o"));
-  }, []);
-
-  return (
-    <i
-      onClick={props.onClick}
-      className={classes}
-      style={{ cursor: "pointer" }}
-      aria-hidden="true"
-    />
-  );
+class Like extends Component {
+  render() {
+    let classes = "fa fa-heart";
+    if (!this.props.liked) classes += "-o";
+    return (
+      <i
+        onClick={this.props.onClick}
+        style={{ cursor: "pointer" }}
+        className={classes}
+        aria-hidden="true"
+      ></i>
+    );
+  }
 }
 
 export default Like;
