43531939fa7ebcd2dc0091de2155ed8737457347
HasanAhmed987 <92812904+HasanAhmed987@users.noreply.github.com>
2022-02-15 04:43:30 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Customers from "./components/customers";
 import Rentals from "./components/rentals";
 import NotFound from "./components/notFound";
 import NavBar from "./components/NavBar";
+import LoginForm from "./components/loginForm";
 import "./App.css";
 
 function App() {
@@ -14,6 +15,7 @@ function App() {
       <NavBar />
       <main className="container">
         <Switch>
+          <Route path="/login" component={LoginForm}></Route>
           <Route path="/movies/:id" component={MovieForm}></Route>
           <Route path="/movies" component={Movies}></Route>
           <Route path="/customers" component={Customers}></Route>
