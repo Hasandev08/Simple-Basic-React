2c251e71ef832ee9acd4d16b945aff7d7d6d02a4
HasanAhmed987 <92812904+HasanAhmed987@users.noreply.github.com>
2022-02-14 18:14:07 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,29 @@
-import './App.css';
-import Movies from './components/Movies';
+import React from "react";
+import { Route, Redirect, Switch } from "react-router-dom";
+import Movies from "./components/Movies";
+import MovieForm from "./components/movieForm";
+import Customers from "./components/customers";
+import Rentals from "./components/rentals";
+import NotFound from "./components/notFound";
+import NavBar from "./components/NavBar";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <Movies />
-    </div>
+    <React.Fragment>
+      <NavBar />
+      <main className="container">
+        <Switch>
+          <Route path="/movies/:id" component={MovieForm}></Route>
+          <Route path="/movies" component={Movies}></Route>
+          <Route path="/customers" component={Customers}></Route>
+          <Route path="/rentals" component={Rentals}></Route>
+          <Route path="/not-found" component={NotFound}></Route>
+          <Redirect from="/" exact to="/movies" />
+          <Redirect to="/not-found" />
+        </Switch>
+      </main>
+    </React.Fragment>
   );
 }
 
