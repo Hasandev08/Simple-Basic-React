a02bb3517fe492a97d3f630336dd53d8d9a2a539
HasanAhmed987 <92812904+HasanAhmed987@users.noreply.github.com>
2022-02-19 01:45:14 +0500

    Add files via upload

---
+++ src/components/register.jsx
@@ -0,0 +1,36 @@
+import React, { Component } from "react";
+import Joi from "joi-browser";
+import Form from "./common/form";
+
+class Register extends Form {
+  state = {
+    data: { username: "", password: "", name: "" },
+    errors: {},
+  };
+
+  schema = {
+    username: Joi.string().required().label("Username"),
+    password: Joi.string().required().label("Password"),
+    name: Joi.string().required().label("Name"),
+  };
+
+  doSubmit = () => {
+    console.log("Submitted");
+  };
+
+  render() {
+    return (
+      <div>
+        <h1>Register</h1>
+        <form onSubmit={this.handleSubmit}>
+          {this.renderInput("username", "Username")}
+          {this.renderInput("password", "Password", "password")}
+          {this.renderInput("name", "Name")}
+          {this.renderButton("Register")}
+        </form>
+      </div>
+    );
+  }
+}
+
+export default Register;
